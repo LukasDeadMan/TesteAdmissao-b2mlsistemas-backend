7ef299cba8b184a8ed467c99890d9d16864c272f
Lucas Soares de Oliveira <lukas.s.o@hotmail.com>
2020-11-28 01:03:23 -0300

    Initial Commit

---
+++ src/main/java/com/lucas/testeadmissao/TesteadmissaoApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.testeadmissao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteadmissaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteadmissaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/testeadmissao/TesteadmissaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.testeadmissao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteadmissaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
